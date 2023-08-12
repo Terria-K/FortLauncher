4241129971ad4f1679e31e9acaee12033c526e49
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-08-12 18:33:00 +0800

    Removed unusued function

--- Launcher/Widgets/Mods.cs
+++ Launcher/Widgets/Mods.cs
@@ -187,25 +187,6 @@ public partial class Launcher
     {
         urlBuilder.Append($"&itemtype[{index}]=Mod&itemid[{index}]={array[1].AsInt32}&fields[{index}]=name,Owner().name,description,downloads,Preview().sSubFeedImageUrl(),Url().sDownloadUrl()");
     }
-
-    public static Texture2D CreateTexture(GraphicsDevice device, int width, int height, Func<int, Color> paint)
-    {
-        //initialize a texture
-        var texture = new Texture2D(device, width, height);
-
-        //the array holds the color for each pixel in the texture
-        Color[] data = new Color[width * height];
-        for(var pixel = 0; pixel < data.Length; pixel++)
-        {
-            //the function applies the color according to the specified pixel
-            data[pixel] = paint( pixel );
-        }
-
-        //set the color
-        texture.SetData( data );
-
-        return texture;
-    }
 }
 
 
